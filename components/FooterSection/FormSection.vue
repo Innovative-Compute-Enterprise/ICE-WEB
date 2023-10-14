17951f597e25d9a84b65fee9a11a2d925f09ece6
GabrielMendonca1 <garimeipod@gmail.com>
2023-10-14 19:41:05 -0300

    V.1.0.6

--- components/FooterSection/FormSection.vue
+++ components/FooterSection/FormSection.vue
@@ -3,13 +3,13 @@
     <h1>Schedule a demo by submitting your contact information</h1>
     <form class="space-y-4">
       <div class="flex flex-col">
-        <input type="text" id="name" name="name" placeholder="Your Name" minlength="5" required class="text-input p-8 rounded-md">
+        <input type="text" id="name" name="name" placeholder="Your Name" minlength="5" required class="text-input p-6 rounded-md">
       </div>
       <div class="flex flex-col">
-        <input type="text" id="company" name="company" placeholder="Your Company" required class="text-input p-8 rounded-md">
+        <input type="text" id="company" name="company" placeholder="Your Company" required class="text-input p-6 rounded-md">
       </div>
       <div class="flex flex-col">
-        <input type="email" id="email" name="email" placeholder="Your Email" minlength="5" required class="text-input p-8 rounded-md">
+        <input type="email" id="email" name="email" placeholder="Your Email" minlength="5" required class="text-input p-6 rounded-md">
       </div>
       <button type="submit" class="bg-black text-white p-6 w-full rounded-md hover:bg-gray-800">BOOK A DEMO</button>
     <div>
