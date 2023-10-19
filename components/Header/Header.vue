ee529f02a4175d4f9bb2aba9a69aa58e2a81399b
GabrielMendonca1 <garimeipod@gmail.com>
2023-10-19 20:15:42 -0300

    V.1.0.6 fixed header bug in iphone safari

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,23 +1,27 @@
 <template>
-  <header class="fixed top-0 w-full bg-opacity-0">
-    <div class="font">
-      <div class="headerA container max-w-full flex justify-between items-center px-4 md:px-8 py-4 ">
-        <Logo />
-        <div class="hidden md:flex bg-black p-[8px] rounded-lg items-center justify-center">
-          <ContactButton />
-          <NavDropdown />
+  <header class="fixed top-0 w-full bg-opacity-0 z-50">
+    <div class="font fixed-wrapper">
+      <div class="fixed-content">
+        <!-- Desktop Header -->
+        <div class="headerA container max-w-full flex justify-between items-center px-4 md:px-8 py-4 ">
+          <Logo />
+          <div class="hidden md:flex bg-black p-[8px] rounded-lg items-center justify-center">
+            <ContactButton />
+            <NavDropdown />
+          </div>
+        </div>
+        <!-- Mobile Menu fixed at bottom -->
+        <div class="flex flex-col headerM justify-center pb-6">
+          <div v-cloak class="md:hidden fixed bottom-4 left-4 right-4 flex bg-black p-[8px] rounded-lg items-center justify-between align-bottom ">
+            <ContactButton />
+            <NavDropdown />
+          </div>
         </div>
       </div>
-      <!-- Mobile Menu fixed at bottom -->
-      <div class=" flex flex-col headerM justify-center pb-6">
-      <div v-cloak class="md:hidden fixed bottom-4 left-4 right-4 flex bg-black p-[8px] rounded-lg items-center justify-between align-bottom ">
-        <ContactButton />
-        <NavDropdown />
-      </div>
-    </div>
     </div>
   </header>
 </template>
+
 
 <script setup>
 // Import the components
@@ -45,16 +49,26 @@ onMounted(() => {
 </script>
 
 <style scoped>
-header{
-  z-index: 1000;
-}
+/* existing styles */
 .font{
   font-family: 'Helvetica Neue Medium', sans-serif;
 }
 
-@media (min-width: 768px) {
-  .headerM{
-    display: -webkit-inline-flex;
-  }
+/* iOS Safari scrolling fix */
+.fixed-wrapper {
+  width: 100%;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  height: 100vh;
+  overflow: scroll;
+  height: -webkit-fill-available;
+  -webkit-overflow-scrolling: touch;
+}
+
+.fixed-wrapper .fixed-content {
+  min-height: 100vh;
 }
 </style>
+
