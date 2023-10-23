837e5d78443bcb942b409d6a9b66fc733b1334a1
GabrielMendonca1 <garimeipod@gmail.com>
2023-10-23 00:20:07 -0300

    V.2.0.6 new header - still need styling

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,28 +1,30 @@
 <template>
-  <header class="fixed top-0 left-0 w-full bg-transparent text-white z-50 flex justify-between items-center px-6 py-2">
-    <!-- Logo -->
-    <div class="text-left">
-      <img src="/logo.png" alt="My Website Logo" class="h-15 w-15">
-    </div>
-    <!-- Hamburger Menu -->
-    <div class="relative" @click="toggleMenu" :class="{ 'z-40': menuOpen }">
-      <input type="checkbox" class="absolute opacity-0 w-0 h-0" v-model="menuOpen" />
+<header class="fixed top-0 left-0 w-full bg-transparent text-white z-50 flex justify-between items-center px-6 py-2">
+  <!-- Logo -->
+  <div class="text-left">
+    <img src="/logo.png" alt="My Website Logo" class="h-15 w-15">
+  </div>
+  <!-- Hamburger Menu -->
+  <div class="relative" @click="toggleMenu" :class="{ 'z-40': menuOpen }">
+    <input type="checkbox" class="absolute opacity-0 w-0 h-0" v-model="menuOpen" />
+    <div ref="iconContainer">
       <!-- SVG for menu -->
-      <svg v-if="!menuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+      <svg v-if="!menuOpen" ref="menuIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 cursor-pointer">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
       </svg>
       <!-- SVG for close -->
-      <svg v-if="menuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+      <svg v-if="menuOpen" ref="closeIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 cursor-pointer">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
     </div>
-    <!-- Slide-in Menu -->
-    <div ref="menu" class="fixed top-0 right-0 w-64 h-screen bg-gray-700 text-white p-4 transform" :class="{ 'translate-x-full': !menuOpen }">
-      <ul class="flex flex-col items-center justify-center h-full space-y-8 text-2xl">
-        <li><a href="#" class="hover:text-blue-600">Home</a></li>
-        <li><a href="#" class="hover:text-blue-600">About</a></li>
-        <li><a href="#" class="hover:text-blue-600">Services</a></li>
-        <li><a href="#" class="hover:text-blue-600">Contact</a></li>
+  </div>
+  <!-- Slide-in Menu -->
+  <div ref="menu" class="fixed top-0 right-0 w-64 h-screen bg-gray-700 text-white p-4 transform" :class="{ 'translate-x-full': !menuOpen }">
+    <ul class="flex flex-col items-center justify-center h-full space-y-8 text-2xl">
+        <li  @mouseenter="addHoverEffect" @mouseleave="removeHoverEffect"><a href="#" class="hover:text-blue-600">Home</a></li>
+        <li  @mouseenter="addHoverEffect" @mouseleave="removeHoverEffect"><a href="#" class="hover:text-blue-600">About</a></li>
+        <li  @mouseenter="addHoverEffect" @mouseleave="removeHoverEffect"><a href="#" class="hover:text-blue-600">Services</a></li>
+        <li  @mouseenter="addHoverEffect" @mouseleave="removeHoverEffect"><a href="#" class="hover:text-blue-600">Contact</a></li>
       </ul>
     </div>
   </header>
@@ -39,14 +41,13 @@ export default {
     };
   },
   mounted() {
-    // Set initial state of the menu to be hidden
     gsap.set(this.$refs.menu, { x: '100%' });
-    
     this.menuTimeline = gsap.timeline({ paused: true })
-      .to(this.$refs.menu, { x: '0%', duration: 0.5, ease: 'power1.out' });
+      .to(this.$refs.menu, { x: '0%', duration: 0.5, ease: 'power2.out' });
   },
   watch: {
     menuOpen(newValue) {
+      this.animateIcon(newValue);
       if (newValue) {
         this.menuTimeline.play();
       } else {
@@ -57,11 +58,36 @@ export default {
   methods: {
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
+    },
+    animateIcon(open) {
+      const target = open ? this.$refs.closeIcon : this.$refs.menuIcon;
+      gsap.to(target, { rotation: open ? -90 : 90, duration: 0.3 });
+    },
+    addHoverEffect(event) {
+      const line = document.createElement('li');
+      line.className = 'hover-line';
+      event.currentTarget.appendChild(line);
+      gsap.to(line, { scaleX: 1, duration: 0.3, ease: 'power2.out' });
+    },
+    removeHoverEffect(event) {
+      const line = event.currentTarget.querySelector('.hover-line');
+      if (line) {
+        gsap.to(line, { scaleX: 0, duration: 0.3, ease: 'power2.in', onComplete: () => line.remove() });
+      }
     }
   }
 };
 </script>
 
 <style scoped>
-/* Custom transition and transformation styles can be added here if needed */
+.hover-line {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: blue;
+  transform-origin: left;
+  transform: scaleX(0);
+}
 </style>
