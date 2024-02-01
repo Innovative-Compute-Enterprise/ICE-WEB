da6fce21548c0fd68dfb3b4a4a3dbc5e5fc38dac
GabrielMendonca1 <garimeipod@gmail.com>
2024-02-01 17:13:19 -0300

    V.1.0.3 - 3.fix1

--- components/About/ContextSection.vue
+++ components/About/ContextSection.vue
@@ -9,9 +9,6 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
 
-import WhiteSection1 from './WhiteSection1.vue';
-import WhiteSection2 from './WhiteSection2.vue';
-
 const BlueSection = defineAsyncComponent(() => import('@//BlueSection.vue'));
 const WhiteSection1 = defineAsyncComponent(() => import('@/WhiteSection1.vue'));
 const WhiteSection2 = defineAsyncComponent(() => import('@/WhiteSection2.vue'));
